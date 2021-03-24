10ce285b734cf15975056f77c0d16b9d359fa32b
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-03-23 23:52:43 -0400

    added comments to models/index for table relationship setup

--- models/index.js
+++ models/index.js
@@ -2,4 +2,12 @@ const User = require('./User');
 const Task = require('./Task');
 const Project = require('./Project');
 
+// Need to define model relationships
+
+// Project has many Tasks
+// Tasks belong to User
+// Tasks belong to Project
+// User has many Tasks
+
+
 module.exports = { User, Task, Project };
