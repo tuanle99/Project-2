9ea37811885511ac8ec8a6fffd4f56539a15b81b
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-03-28 13:07:36 -0400

    wrote initial model relationships

--- models/index.js
+++ models/index.js
@@ -4,10 +4,33 @@ const Project = require('./Project');
 
 // Need to define model relationships
 
-// Project has many Tasks
-// Tasks belong to User
-// Tasks belong to Project
-// User has many Tasks
+// Project / User relationships
+User.hasMany(Project, {
+    foreignKey: 'user_id'
+});
 
+Project.belongsTo(User, {
+    foreign_key: 'user_id'
+});
 
+// Tasks / User relationships
+User.hasMany(Task, {
+    foreignKey: 'user_id'
+});
+
+Task.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// Project / Task relationships
+Project.hasMany(Task, {
+    foreignKey: 'project_id',
+    onDelete: 'CASCADE'
+});
+
+Task.belongsTo(Project, {
+    foreignKey: 'project_id'
+});
+
+// Export for use in controllers
 module.exports = { User, Task, Project };
