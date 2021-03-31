33acf05fe6b9c7b86677eb20081b030cbbfb386b
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-03-30 22:18:55 -0400

    setup file structure for task routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -7,6 +7,7 @@ const userRoutes = require('./userRoutes');
 const profileRoutes = require('./profileRoutes');
 const projectRoutes = require('./projectRoutes');
 const budgetRoutes = require('./budgetRoutes');
+const taskRoutes = require('./taskRoutes');
 
 /* -------------------------------------------------------------------------- */
 /*                       Define Middleware For This Path                      */
@@ -17,6 +18,7 @@ router.use('/users', userRoutes); // any path with /api/users use userRoutes tha
 router.use('/profile', profileRoutes);
 router.use('/project', projectRoutes);
 router.use('/budget', budgetRoutes);
+router.use('/tasks', taskRoutes);
 
 /* -------------------------------------------------------------------------- */
 /*                                Export Module                               */
---
+++ controllers/api/taskRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Task } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newTask = await Task.create({
+            ...req.body,
+            created_by: req.session.user_id,
+        });
+
+        res.status(200).json(newTask);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const taskData = await Task.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog found with this id!' });
+            return;
+        }
+
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
