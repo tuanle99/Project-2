60b74e484980c9c9922af4a6cfe44d584f513f3a
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-03-31 21:53:45 -0400

    post and delete routes working for budget page

--- controllers/api/budgetRoutes.js
+++ controllers/api/budgetRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 var sequelize = require('sequelize');
 const { Budget } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // Render profile page
 // Need update to current user after log in
@@ -21,6 +22,52 @@ router.get('/', async (req, res) => {
         });
     }
     catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Posting a new expense item to the db
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newExpense = await Budget.create({
+            ...req.body
+        });
+
+        res.status(200).json(newExpense);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.put('/:id', withAuth, (req, res) => {
+    // update a budget by its `id` value
+    Budget.update(req.body, {
+        where: {
+            id: req.params.id,
+        },
+    })
+        .then((updatedBudget) => res.json(updatedBudget))
+        .catch((err) => {
+            // console.log(err);
+            res.status(400).json(err);
+        });
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const budgetData = await Budget.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (!budgetData) {
+            res.status(404).json({ message: 'No expense found with this id!' });
+            return;
+        }
+
+        res.status(200).json(budgetData);
+    } catch (err) {
         res.status(500).json(err);
     }
 });
